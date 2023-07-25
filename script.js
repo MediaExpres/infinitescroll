ae0b0234557f50d72d1e467d22b8d18186f0230f
Mihai Pavelescu <media.expres@gmail.com>
2023-07-25 18:23:16 +0300

    created imageContainer and loader constants

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 import { apiKey } from "./config.js";
 
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unsplash API
 
 const count = 10;
