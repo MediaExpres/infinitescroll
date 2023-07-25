aba55b3cc79654002ae181e143a72cfbcc16f379
Mihai Pavelescu <media.expres@gmail.com>
2023-07-25 15:08:35 +0300

    hidding the apykey from github

---
+++ script.js
@@ -0,0 +1,26 @@
+import { apiKey } from "./config.js";
+
+// Unsplash API
+
+const count = 10;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+
+// Get photos from Unsplash API 
+
+async function getPhotos() {
+    try {
+const response = await fetch(apiUrl);
+const data = await response.json();
+console.log(data);
+
+    } catch (error) {
+        // Catch error here
+    }
+}
+
+
+// On Load
+
+getPhotos();
