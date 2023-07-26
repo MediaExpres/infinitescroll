80c667b365ce3f81929d3f6a694bb6a2c90995af
Mihai Pavelescu <media.expres@gmail.com>
2023-07-26 15:34:53 +0300

    added if statement for loading images

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import { apiKey } from "./config.js";
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
@@ -14,6 +17,11 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 // Check if all images loaded
 function imageLoaded() {
     console.log('image loaded');
+    imagesLoaded++; // images increase by one
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        console.log('ready =', ready);
+    }
 }
 
 function setAttributes(element, attributes) {
@@ -25,6 +33,9 @@ function setAttributes(element, attributes) {
 // Create Elements For Links & Photos, and add that to the DOM
 
 function displayPhotos() {
+    totalImages = photosArray.length; 
+    console.log('total images: ', totalImages);
+    
     // Run for each object in photosArray
     photosArray.forEach((photo) => {
       // create <a></a> element to link to Unsplash
